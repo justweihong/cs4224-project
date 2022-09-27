0ad500a1b68ed578e8ea2d60dd1f464263dd5cf4
justweihong <justweihong>
2022-09-27 20:54:46 +0800

    Update script path

--- ysql/setup/ysqlsetup.sql
+++ ysql/setup/ysqlsetup.sql
@@ -4,5 +4,5 @@
 \copy customers from './project_files/data_files/customer.csv'with (format csv, delimiter ',', null 'null', rows_per_transaction 1000, disable_fk_check);
 \copy orders from './project_files/data_files/order.csv' with (format csv, delimiter ',', null 'null', rows_per_transaction 1000, disable_fk_check);
 \copy items from './project_files/data_files/item.csv' with (format csv, delimiter ',', null 'null', rows_per_transaction 1000, disable_fk_check);
-\copy order_lines from './project_files/data_files/order_line.csv' with (format csv, delimiter ',', null 'null', rows_per_transaction 1000, disable_fk_check);
+\copy order_lines from './project_files/data_files/order-line.csv' with (format csv, delimiter ',', null 'null', rows_per_transaction 1000, disable_fk_check);
 \copy stocks from './project_files/data_files/stock.csv' with (format csv, delimiter ',', null 'null', rows_per_transaction 1000, disable_fk_check);    
