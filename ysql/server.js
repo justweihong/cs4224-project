2c0b6f7f42022a2a38ff08c7bbf163f945376e26
justweihong <justweihong>
2022-10-04 00:38:20 +0800

    Add parser for client input

--- ysql/server.js
+++ ysql/server.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const { callbackify } = require('util');
 const { rows } = require('pg/lib/defaults');
 
+// Config
 const config = {
     host: '127.0.0.1',
-    port: '5433',
+    port: '9999',
     database: 'yugabyte',
     user: 'yugabyte',
     password: 'yugabyte',
@@ -17,6 +18,18 @@ const config = {
     // },
     connectionTimeoutMillis: 5000
 };
+
+// Transaction Types
+const TransactionTypes = {
+    NEW_ORDER: 'N',
+    PAYMENT: 'P',
+    DELIVERY: 'D',
+    ORDER_STATUS: 'O',
+    STOCK_LEVEL: 'S',
+    POPULAR_ITEM: 'I',
+    TOP_BALANCE: 'T',
+    RELATED_CUSTOMER: 'R'
+}
 
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -43,152 +56,81 @@ async function connect(callbackHadler) {
     }
 }
 
-async function newOrderTransaction(callbackHadler, W_ID, D_ID, C_ID, NUM_ITEMS, ITEM_NUMBER, SUPPLIER_WAREHOUSE, QUANTITY) {
+async function parser(callbackHadler, filePath) {
+    fs.readFile(filePath, 'utf8', function (err,data) {
 
-    //STEP 1
-    let N = await client.query('SELECT D_NEXT_O_ID FROM District WHERE W_ID = ' + W_ID + ' AND D_ID = ' + D_ID);
-   
-    //STEP 2
-    let nPlusOne = N + 1; 
-    await client.query('UPDATE District SET D_NEXT_O_ID = ' + nPlusOne);
-   
-    //STEP 3
-    let date_obj = Date.now();
-    var O_ALL_LOCAL = 1;
-    for (var i = 1; i <= SUPPLIER_WAREHOUSE.length; i++) {
-     if (SUPPLIER_WAREHOUSE[i] != W_ID) {
-      O_ALL_LOCAL = 0;
-     }
+    // Return error for invalid file
+    if (err) {
+        callbackHadler(err);
     }
-    var createNewOrderStmt = 'INSERT INTO Order VALUES (' + N + ',' + D_ID + ',' + W_ID + ',' + C_ID + ',' + date_obj + ',' + NULL + ',' + NUM_ITEMS + ',' + O_ALL_LOCAL + ')';
-    await client.query(createNewOrderStmt);
-   
-    //STEP 4
-    var TOTAL_AMOUNT = 0;
-   
-    //STEP 5
-    for (var i = 1; i <= NUM_ITEMS; i++) {
-   
-     //PART A
-     let ITEM_NO = ITEM_NUMBER[i];
-     let WAREHOUSE = SUPPLIER_WAREHOUSE[i];
-     let S_QUANTITY = await client.query('SELECT S_QUANTITY FROM Stock WHERE S_W_ID = ' + WAREHOUSE + ' AND S_I_ID = ' + ITEM_NO);
-   
-     //PART B
-     var ADJUSTED_QTY = S_QUANTITY - QUANTITY[i];
-     
-     //PART C
-     if (ADJUSTED_QTY < 10) {
-      ADJUSTED_QTY = ADJUSTED_QTY + 100;
-     }
-   
-     //PART D
-     var updateStockStmt = "";
-     if (SUPPLIER_WAREHOUSE[i] != W_ID) {
-      updateStockStmt = 'UPDATE Stock SET S_QUANTITY = ' + ADJUSTED_QTY + 
-      ', S_YTD = S_YTD + ' + QUANTITY[i] + ', S_ORDER_CNT = S_ORDER_CNT + 1, S_REMOTE_CNT = S_REMOTE_CNT + 1 WHERE S_W_ID = ' + 
-      WAREHOUSE + ' AND S_I_ID = ' + ITEM_NO;
-     } else {
-      updateStockStmt = 'UPDATE Stock SET S_QUANTITY = ' + ADJUSTED_QTY + 
-      ', S_YTD = S_YTD + ' + QUANTITY[i] + ', S_ORDER_CNT = S_ORDER_CNT + 1 WHERE S_W_ID = ' + 
-      WAREHOUSE + ' AND S_I_ID = ' + ITEM_NO;
-     }
-     await client.query(updateStockStmt);
-   
-     //PART E
-     var I_PRICE = await client.query('SELECT I_PRICE FROM Item WHERE I_ID = ' + ITEM_NO);
-     var ITEM_AMOUNT = QUANTITY[i] * I_PRICE;
-   
-     //PART F
-     TOTAL_AMOUNT = TOTAL_AMOUNT + ITEM_AMOUNT;
-   
-     //PART G
-     var OL_DIST_INFO = await client.query('SELECT S_DIST_'+ D_ID + ' FROM Stock');
-     var createNewOrderLineStmt = 'INSERT INTO Order-Line VALUES (' + N + ',' + D_ID + ',' + W_ID + ',' + i + ',' + ITEM_NO + ',' + WAREHOUSE + ',' + QUANTITY[i] + ',' + ITEM_AMOUNT + ', NULL,' +  OL_DIST_INFO;
-     await client.query(createNewOrderLineStmt);
-    }
-   
-   
-    //STEP 6
-    var W_TAX = await client.query('SELECT W_TAX FROM Warehouse WHERE W_ID = ' + W_ID);
-    var D_TAX = await client.query('SELECT D_TAX FROM District WHERE D_ID = ' + D_ID);
-    var C_DISCOUNT = await client.query('SELECT C_DISCOUNT FROM Customer WHERE C_ID = ' + C_ID);
-    TOTAL_AMOUNT = TOTAL_AMOUNT * (1 + D_TAX + W_TAX) * (1 - C_DISCOUNT);
-   
+
+    const lines = data.split(/\r?\n/);
+
+    // Variables for NewOrderTransaction
+    let orderDetails;
+    let itemDetails = [];
+    let itemsLeft = -1;
     
-    console.log('>>>> NEW ORDER TRANSACTION');
-   
-    //OUTPUT STEP 1
-    var C_LAST = await client.query('SELECT C_LAST FROM Customer WHERE C_ID = ' + C_ID);
-    var C_CREDIT = await client.query('SELECT C_CREDIT FROM Customer WHERE C_ID = ' + C_ID); 
-    console.log('Customer Identifier(' + W_ID + ',' + D_ID + ',' + C_ID + '), LastName ' + C_LAST + ', Credit ' + C_CREDIT + ', Discount '  + C_DISCOUNT);
-   
-    //OUTPUT STEP 2
-    console.log('Warehouse Tax Rate ' + W_TAX + ', District Tax Rate ' + D_TAX);
-   
-    //OUTPUT STEP 3
-    console.log('Order Number ' + N + ', Entry Date ' + date_obj);
-     
-    //OUTPUT STEP 4
-    console.log('Number of Items ' + NUM_ITEMS + ', Total Amount for Order ' + TOTAL_AMOUNT);
-   
-    //OUTPUT STEP 5
-    for (var i = 1; i < NUM_ITEMS; i++) {
-     var ITEM_NO = ITEM_NUMBER[i];
-     var I_NAME = await client.query('SELECT I_NAME FROM Item WHERE I_ID = ' + ITEM_NO);
-     var I_PRICE = await client.query('SELECT I_PRICE FROM Item Where I_ID = ' + ITEM_NO);
-     var OL_AMOUNT = QUANTITY[i] * I_PRICE;
-     var S_QUANTITY = await client.query('SELECT S_QUANTITY FROM Stock WHERE S_W_ID = ' + W_ID + ' AND S_I_ID = ' + ITEM_NO);
-   
-     console.log(ITEM_NO + ', ' + I_NAME + ', ' + SUPPLIER_WAREHOUSE[i] + ', ' + QUANTITY[i] + ', ' + OL_AMOUNT + ', ' + S_QUANTITY);    
-    }
-}
+    lines.forEach(line => {
+        let args = line.split(',');
 
-async function wrapper(callbackHadler) {
-    readline.question('', function(input) {
-        let args = input.split(',');
-        if (args[0] == 'N') {
-            console.log('Running New Transaction Statement, Arguments:');
-            console.log('W_ID: ' + args[1]);
-            console.log('D_ID: ' + args[2]);
-            console.log('C_ID: ' + args[3]);
-            console.log('Number of Items: ' + args[4]);
+        // if N, set itemleft = n, then if itemleft >0 add item, if 0, return and make -1
 
-            var itemsToProcess = [];
-            for (var i=0; i < args[4]; i++) {
-                readline.question('', function(item) {
-                    itemsToProcess.push(item);
-                });
-            }
 
-            console.log(itemsToProcess);
-        } else if (args[0] == 'P') {
 
-        } else if (args[0] == 'D') {
-
-        } else if (args[0] == 'O') {
-
-        } else if (args[0] == 'S') {
-
-        } else if (args[0] == 'I') {
-
-        } else if (args[0] == 'R') {
-
-        } else {
-
+        
+        // Item line, add into items for new order transaction
+        if (itemsLeft > 0) {
+            itemDetails.push(args);
+            itemsLeft--;
+        
+        // End of item lines, execute new order transaction
+        } else if (itemsLeft == 0) {
+            // TODO: execute new order transaction with orderDetails and itemDetails
+            itemsLeft--;
         }
-        readline.close();
+
+        //The first value in the first line must be either N, P, D, O, S, I, T, or R, denoting, respectively, a 
+        // new order, payment, delivery, order-status, stock-level, popular-item, top-balance, or related-customer transaction
+
+        switch(args[0]) {
+            case TransactionTypes.NEW_ORDER:
+                orderDetails = args.slice(1);
+                itemsLeft = args[4];
+                break;
+
+            // case TransactionTypes.PAYMENT: // TODO
+            // case TransactionTypes.DELIVERY: // TODO
+            case TransactionTypes.ORDER_STATUS:
+                console.log(args);
+                break;
+
+            // case TransactionTypes.STOCK_LEVEL: // TODO
+            // case TransactionTypes.POPULAR_ITEM:
+            // case TransactionTypes.TOP_BALANCE:
+            // case TransactionTypes.RELATED_CUSTOMER:
+        }
+        
+        
+        
+        // console.log(args);
+
+    })
+
+    // console.log(data);
+    callbackHadler();
     });
 }
 
 
 async.series([
+    // function (callbackHadler) {
+    //     connect(callbackHadler);
+    // },
     function (callbackHadler) {
-        wrapper(callbackHadler);
+        parser(callbackHadler, '../project_files/xact_files/0.txt');
     },
-    function (callbackHadler) {
-        connect(callbackHadler);
-    },
+    
 ],
     function (err) {
         if (err) {
