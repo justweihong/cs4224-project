6b9b8a46dc4e0f81e4f0661919b8093ecc44ddc9
Maxie <mpang45456@gmail.com>
2022-10-16 05:56:57 +0800

    Added NewOrder, OrderStatus, and StockLevel to Ycql

---
+++ ycql/transactions/StockLevelTransaction.js
@@ -0,0 +1,20 @@
+async function stockLevelTransaction(client, W_ID, D_ID, T, L) {
+    const orderQuery = 'SELECT O_ID FROM orders' + 
+        ' WHERE O_W_ID = ' + W_ID + ' AND O_D_ID = ' + D_ID +
+        ' ORDER BY O_ENTRY_D DESC LIMIT ' + L + ';';
+    const lastOrders = (await client.query(orderQuery)).rows; 
+    const lastOrderIds = '(' + lastOrders.map((x) => x.o_id).toString() + ')' // Order IDs to parse in SQL IN query
+
+    const itemQuery = 'SELECT DISTINCT OL_I_ID FROM order_lines' + 
+        ' WHERE OL_W_ID = ' + W_ID + ' AND OL_D_ID = ' + D_ID +
+        ' AND OL_O_ID IN ' + lastOrderIds;
+    const items = (await client.query(itemQuery)).rows;
+    const itemIds = '(' + items.map((x) => x.ol_i_id).toString() + ')' // Order IDs to parse in SQL IN query
+
+    const stockQuery = 'SELECT COUNT(S_I_ID) FROM stocks' + 
+        ' WHERE S_W_ID = ' + W_ID + ' AND S_I_ID IN ' + itemIds + ' AND S_QUANTITY < ' + T;
+    const noOfItemsWithLowStock = (await client.query(stockQuery)).rows[0].count;
+    console.log('Total number of items with low stock: %d', noOfItemsWithLowStock);
+}
+
+module.exports = { stockLevelTransaction };
