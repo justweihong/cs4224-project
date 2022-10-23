82cc49974541fba22baae226521c83ca772de5d3
hmtey <huiming.tey@gmail.com>
2022-10-24 00:37:30 +0800

    Fix ycql payment and delivery transaction bugs

--- ycql/transactions/deliveryTransaction.js
+++ ycql/transactions/deliveryTransaction.js
@@ -1,3 +1,5 @@
+const BigDecimal = require('cassandra-driver').types.BigDecimal;
+
 async function deliveryTransaction(client, w_id, carrier_id) {
     if (carrier_id < 1 || carrier_id > 10) {
         console.error('Carrier ID does not fall betwwen the range of [1,10].');
@@ -8,7 +10,7 @@ async function deliveryTransaction(client, w_id, carrier_id) {
             var cust_no;
 
             await client
-                .execute('SELECT MIN(o_id) FROM orders WHERE o_w_id = ' + w_id + ' AND o_d_id = ' + (i + 1) + ' AND o_carrier_id IS NULL')
+                .execute('SELECT MIN(o_id) as min FROM orders WHERE o_w_id = ' + w_id + ' AND o_d_id = ' + (i + 1) + ' AND o_carrier_id = null')
                 .then(res => {
                     order_no = res.rows[0].min;
                 })
@@ -36,31 +38,57 @@ async function deliveryTransaction(client, w_id, carrier_id) {
 
             // PROCESS 1b
             await client
-                .execute('UPDATE orders SET o_carrier_id = ' + carrier_id + ' WHERE o_id = ' + order_no)
+                .execute('UPDATE orders SET o_carrier_id = ' + carrier_id + ' WHERE o_w_id = ' + w_id + ' AND o_d_id = ' + (i + 1) + ' AND o_id = ' + order_no)
                 .catch(err => {
                     console.error(err.stack);
                 })
 
             // PROCESS 1c
+            var OL_NOS;
             await client
-                .execute('UPDATE order_lines SET ol_delivery_d = toTimestamp(now()) WHERE ol_o_id = ' + order_no)
+                .execute('SELECT ol_number from order_lines WHERE ol_w_id = ' + w_id + ' AND ol_d_id = ' + (i + 1) + ' AND ol_o_id = ' + order_no)
+                .then(res => {
+                    OL_NOS = res;
+                })
                 .catch(err => {
                     console.error(err.stack);
                 })
+            
+            for (j = 0; j < OL_NOS.length; j++) {
+                await client
+                .execute('UPDATE order_lines SET ol_delivery_d = toTimestamp(now()) WHERE ol_w_id = ' + w_id 
+                    + ' AND ol_d_id = ' + (i + 1) + ' AND ol_o_id = ' + order_no + 'AND ol_number = ' + OL_NOS[j].ol_number)
+                .catch(err => {
+                    console.error(err.stack);
+                })
+            }
 
             // PROCESS 1d
-            var balance
+            var SUM_DECIMAL;
+            var NEW_C_BALANCE;
+            var NEW_C_DELIVERY_CNT;
             await client
-                .execute('SELECT SUM(ol_amount) FROM order_lines WHERE ol_o_id = ' + order_no)
+                .execute('SELECT SUM(ol_amount) as sum FROM order_lines WHERE ol_o_id = ' + order_no)
                 .then(res => {
-                    balance = res.rows[0].sum;
+                    SUM_DECIMAL = BigDecimal.fromNumber(parseFloat(res.rows[0].sum));
                 })
                 .catch(err => {
                     console.error(err.stack);
                 })
 
             await client
-                .execute('UPDATE customers SET c_balance = c_balance + ' + balance + ', c_delivery_cnt = c_delivery_cnt + 1 WHERE c_id = ' + cust_no)
+                .execute('SELECT c_balance, c_delivery_cnt from customers WHERE c_id = ' + cust_no)
+                .then(res => {
+                    NEW_C_BALANCE = res.rows[0].c_balance.add(SUM_DECIMAL);
+                    NEW_C_DELIVERY_CNT = res.rows[0].c_delivery_cnt + 1;
+                })
+                .catch(err => {
+                    console.error(err.stack);
+                })
+
+            await client
+                .execute('UPDATE customers SET c_balance = ' + NEW_C_BALANCE + ', c_delivery_cnt = ' + NEW_C_DELIVERY_CNT 
+                    + ' WHERE c_w_id = ' + w_id + ' AND c_d_id = ' + (i + 1) + 'AND c_id = ' + cust_no)
                 .catch(err => {
                     console.error(err.stack);
                 })
--- ycql/transactions/paymentTransaction.js
+++ ycql/transactions/paymentTransaction.js
@@ -1,23 +1,59 @@
+const BigDecimal = require('cassandra-driver').types.BigDecimal;
+
 async function paymentTransaction(client, c_w_id, c_d_id, c_id, payment) {
+    var PAYMENT_DECIMAL = BigDecimal.fromNumber(parseFloat(payment));
+
     // PROCESS 1
+    var NEW_W_YTD;
     await client
-        .execute('UPDATE warehouses SET w_ytd = w_ytd + ' + payment + ' WHERE w_id = ' + c_w_id)
+        .execute('SELECT w_ytd FROM warehouses WHERE w_id = ' + c_w_id)
+        .then(res => {
+            NEW_W_YTD = res.rows[0].w_ytd.add(PAYMENT_DECIMAL);
+        })
+        .catch(err => {
+            console.error(err.stack);
+        })
+        
+    await client
+        .execute('UPDATE warehouses SET w_ytd = ' + NEW_W_YTD + ' WHERE w_id = ' + c_w_id)
         .catch(err => {
             console.error(err.stack);
         })
 
     // PROCESS 2
+    var NEW_D_YTD;
     await client
-        .execute('UPDATE districts SET d_ytd = d_ytd + ' + payment + ' WHERE d_w_id = ' + c_w_id + ' AND d_id = ' + c_d_id)
+        .execute('SELECT d_ytd FROM districts WHERE d_w_id = ' + c_w_id + ' AND d_id = ' + c_d_id)
+        .then(res => {
+            NEW_D_YTD = res.rows[0].d_ytd.add(PAYMENT_DECIMAL);
+        })
+        .catch(err => {
+            console.error(err.stack);
+        })
+    await client
+        .execute('UPDATE districts SET d_ytd = ' + NEW_D_YTD + ' WHERE d_w_id = ' + c_w_id + ' AND d_id = ' + c_d_id)
         .catch(err => {
             console.error(err.stack);
         })
 
     // PROCESS 3
+    var NEW_BALANCE;
+    var NEW_C_YTD;
+    var NEW_PAYMENT_CNT;
     await client
-        .execute('UPDATE customers SET c_balance = c_balance - ' + payment 
-            + ', c_ytd_payment = c_ytd_payment + ' + payment
-            + ', c_payment_cnt = c_payment_cnt + 1 WHERE c_w_id = ' + c_w_id + ' AND c_d_id = ' + c_d_id + ' AND c_id = ' + c_id)
+    .execute('SELECT c_balance, c_ytd_payment, c_payment_cnt FROM customers WHERE c_w_id = ' + c_w_id + ' AND c_d_id = ' + c_d_id + ' AND c_id = ' + c_id)
+    .then(res => {
+        NEW_BALANCE = res.rows[0].c_balance.subtract(PAYMENT_DECIMAL);
+        NEW_C_YTD = res.rows[0].c_ytd_payment + parseFloat(payment);
+        NEW_PAYMENT_CNT = res.rows[0].c_payment_cnt + 1;
+    })
+    .catch(err => {
+        console.error(err.stack);
+    })
+    await client
+        .execute('UPDATE customers SET c_balance = ' + NEW_BALANCE 
+            + ', c_ytd_payment = ' + NEW_C_YTD
+            + ', c_payment_cnt = ' + NEW_PAYMENT_CNT + ' WHERE c_w_id = ' + c_w_id + ' AND c_d_id = ' + c_d_id + ' AND c_id = ' + c_id)
         .catch(err => {
             console.error(err.stack);
         })
