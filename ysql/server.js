782bc3fe6628d5cd7c84d58b58d55c0562177ad3
Pratyush Ghosh <42459327+pratyushghosh@users.noreply.github.com>
2022-10-31 00:12:14 +0800

    Added SoC Cluster Yugabyte configuration to server.js for YSQL

--- ysql/server.js
+++ ysql/server.js
@@ -21,8 +21,8 @@ const { generateDBState } = require('./util/generateDBState');
 
 // Config
 const config = {
-    host: '127.0.0.1',
-    port: '5433',
+    host: '192.168.48.219',
+    port: '6433',
     database: 'supplier_db',
     user: 'yugabyte',
     password: 'yugabyte',
